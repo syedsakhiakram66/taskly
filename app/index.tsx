7675e69f72b26c2126d692691a08b3ca5bd3cda3
Sakhi Akram <sakhiakram.github@outlook.com>
2026-02-04 10:22:47 +0500

    Example of stack navigation with expo router

--- app/index.tsx
+++ app/index.tsx
@@ -1,9 +1,9 @@
 import {StyleSheet, View} from 'react-native';
 import {  ShoppingListItem  } from "../components/ShoppingListItem";
 import { theme } from "../theme";
+import { Link } from "expo-router";
 
-
-export default function App() {
+export default function Index() {
   const handleDelete  = () => {
     Alert.alert(
       "Are you sure you want to delete this?",
@@ -21,6 +21,13 @@ export default function App() {
   }
   return (
     <View style={styles.container}>
+        <Link
+                href="/counter"
+                style={{ textAlign: "center", marginBottom: 18, fontSize: 24 }}
+              >
+                Go to /counter
+           </Link>
+
         <ShoppingListItem name="Coffee" />
         <ShoppingListItem name="Tea"  isCompleted/>
         <ShoppingListItem name="Milk" isCompleted/>
