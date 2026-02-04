e3b99274ca7c9464d35ded4552758ca89a7b6e29
Sakhi Akram <sakhiakram.github@outlook.com>
2026-02-04 10:58:41 +0500

    Example of presentation modal

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -4,6 +4,8 @@ export default function Layout() {
     return (
         <Stack>
             <Stack.Screen name="index" options={{tite: "Shopping List"}} />
+            <Stack.Screen name="counter" options={{tite: "Counter Screen", presentation: "modal", animation: "fade_from_bottom"}} />
+            <Stack.Screen name="idea" options={{title: "My idea", presentation: "modal", animation: "fade_from_bottom"}} />
         </Stack>
     );
 }
